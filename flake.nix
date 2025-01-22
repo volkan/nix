1cbcae5a60eae5db3e7d15a04bb1ee3fdb03ba10
Volkan Altan <volkan.altan@vestiairecollective.com>
2025-01-22 16:35:07 +0100

    update

--- flake.nix
+++ flake.nix
@@ -201,18 +201,7 @@
         enable = true;
         enableCompletion = true;
         promptInit = ""; # Clear this to avoid conflict
-        plugins = [
-          {
-            name = "zsh-nix-shell";
-            file = "nix-shell.plugin.zsh";
-            src = pkgs.fetchFromGitHub {
-              owner = "chisui";
-              repo = "zsh-nix-shell";
-              rev = "v0.8.0";
-              sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
-            };
-          }
-        ];    
+          
         interactiveShellInit = ''
           export HISTFILE="$HOME/.zsh_history"
           export HISTSIZE=10000000
